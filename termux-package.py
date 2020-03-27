ed61a85deaf1a4ff50e27b390d175b7e64fb02b7
requinxcode <62765376+requinxcode@users.noreply.github.com>
2020-03-28 00:59:28 +0700

    Add files via upload

---
+++ termux-package.py
@@ -0,0 +1,7 @@
+print("Tools Install package Termux BY : RXE TEAM")
+print("Masukan nama anda : ",raw_input(nama))
+print("Nama anda : ",nama)
+
+pkg install git
+pkg install ruby
+pkg install python
